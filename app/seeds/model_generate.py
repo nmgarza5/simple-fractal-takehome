bb16b465de68e3d30fe3edec5a143a7b37f37171
Nikolas <nikolas.garza5@gmail.com>
2022-09-01 19:15:10 -0700

    fixing seeding issue

---
+++ app/seeds/model_generate.py
@@ -0,0 +1,13 @@
+from app.models import db, Company, Record
+
+def seed_companies():
+    db.session.commit()
+def seed_records():
+    db.session.commit()
+
+def undo_companies():
+    db.session.execute('TRUNCATE companies RESTART IDENTITY CASCADE;')
+    db.session.commit()
+def undo_records():
+    db.session.execute('TRUNCATE records RESTART IDENTITY CASCADE;')
+    db.session.commit()
