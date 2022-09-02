edce8dccda8428f14a83c4d62f1b2929f6567c59
Nikolas <nikolas.garza5@gmail.com>
2022-09-02 10:20:43 -0700

    init, removing unneccesary files/folders

--- app/__init__.py
+++ app/__init__.py
@@ -3,20 +3,11 @@ from flask import Flask, render_template, request, session, redirect
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect, generate_csrf
-
-
-from .models import db, Record, Company
-
-from .seeds import seed_commands
+from .models import db
 from .api.percentile_routes import percentile_routes
-
 from .config import Config
 
 app = Flask(__name__)
-
-
-# Tell flask about our seed commands
-app.cli.add_command(seed_commands)
 
 app.config.from_object(Config)
 
